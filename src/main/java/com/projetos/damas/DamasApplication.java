97f953c50209a2855a2c944df2710fc0e4349a86
carlos <carloshumberto1990@gmail.com>
2018-07-26 14:50:52 -0300

    Estrutura do projeto

---
+++ src/main/java/com/projetos/damas/DamasApplication.java
@@ -0,0 +1,12 @@
+package com.projetos.damas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DamasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DamasApplication.class, args);
+	}
+}
---
+++ src/test/java/com/projetos/damas/DamasApplicationTests.java
@@ -0,0 +1,16 @@
+package com.projetos.damas;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DamasApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
